8b73f59ea623fb03d9ed46758844b426476bded6
Alexandro Dorta Mirena <alu0101802961@ull.edu.es>
2026-09-15 13:16:14 +0000

    ejercicio terminado parte de casa

---
+++ ejercicio2_corregido/p01_multiple_grades.cc
@@ -0,0 +1,90 @@
+// Universidad de La Laguna
+// Escuela Superior de Ingenieria y Tecnologia
+// Grado en Ingenieria Informatica
+// Asignatura: Computabilidad y Algoritmia
+// Curso: 2º
+// Practica 1: Contenedores asociativos
+// Autor: Alexandro Jesus Dorta Mirena
+// Correo: alu0101802961@ull.edu.es
+// Fecha: 10/09/2026
+// Archivo makefile: makefile.
+// Contiene la funcion main del proyecto que usa las clases X e Y
+// para ... (indicar brevemente el objetivo)
+// Referencias:
+// Enlaces de interes
+// Historial de revisiones
+// 12/09/2023 - Creacion (primera version) del codigo
+
+#include "p01_multiple_grades.h"
+
+void MostrarInformacion(){
+  std::cout << "El programa consiste en un clasificacion de varias notas de alumnos con su alu que se toman de un" << std::endl;
+  std::cout << "fichero aparte, no obstante usted como usuario puede agregar tantos alu como notas como le haga falta " << std::endl;
+}
+
+const std::multimap<std::string, double>& LectorFichero::GetCalificacionLeida() const{
+  return calificaciones_leida_;
+}
+
+const std::map<std::string, std::vector<double>>& GestorNotas::GetCalificacion() const{
+  return calificaciones_;
+}
+
+
+void LectorFichero::LeerFichero(const std::string fichero_leer){
+  std::ifstream fichero_leido(fichero_leer);
+  if(!fichero_leido.is_open()){
+    std::cerr << "fichero no abierto" << std::endl;
+    return;
+  }
+  std::string alu;
+  std::string nota;
+  while(fichero_leido >> alu >> nota){
+    double nota_double = std::stod(nota);
+    calificaciones_leida_.insert(std::make_pair(alu, nota_double));
+  }
+
+  fichero_leido.close();
+}
+
+
+void LectorFichero::MostrarCalificacionLeida(){
+  for(const auto& [alu, nota] : calificaciones_leida_){
+    std::cout << alu << ": " << nota << std::endl;
+  }
+}
+
+void GestorNotas::OrdenarCalificacion(const LectorFichero& Calificacion_leida){
+  for(const auto& [alu, nota] : Calificacion_leida.GetCalificacionLeida()){
+    if(calificaciones_.find(alu) == calificaciones_.end()){
+      std::vector<double> vector_notas;
+      vector_notas.push_back(nota);
+      calificaciones_.insert(std::make_pair(alu, vector_notas));
+    } else{
+        calificaciones_[alu].push_back(nota);
+    }
+  }
+}
+
+void GestorNotas::MostrarCalificacionClasificada(){
+  for(const auto& [alu, nota] : calificaciones_){
+    std::cout << alu << ": ";
+    for(size_t i = 0; i < nota.size(); i++ ){
+      std::cout << nota[i] << " ";
+    }
+    std::cout << std::endl;
+  }
+}
+
+void GestorNotas::InsertarNuevoCalificacion(std::string alu, double nota){
+  if(calificaciones_.find(alu) == calificaciones_.end()){
+    std::vector<double> vector_notas;
+    vector_notas.push_back(nota);
+    calificaciones_.insert(std::make_pair(alu, vector_notas));
+  } else {
+      calificaciones_[alu].push_back(nota);
+  }
+}
+
+
+
